07c7e81b8def01aeef23c285ec092a164d7f9cb9
Sarthak <50234097+Sarthak160@users.noreply.github.com>
2025-05-13 16:44:19 +0530

    Update events.go

--- pkg/http/events.go
+++ pkg/http/events.go
@@ -29,6 +29,8 @@ func NewEventsService(logger *zap.Logger, eventsCollection *mongo.Collection, re
 func (es *EventsService) StoreEventLogs(ctx context.Context, triggerName string, eventLogs models.Events) error {
 	key := createRandomKey(triggerName)
 
+
+	
 	err := es.redisClient.Set(ctx, key, eventLogs, 2*time.Hour).Err()
 	if err != nil {
 		es.logger.Error("error storing event logs", zap.Error(err))
